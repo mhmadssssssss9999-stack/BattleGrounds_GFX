48aa13fa6c10ac6817a0c75155ea358612f2a418
mhmadssssssss9999-stack <mhmadssssssss9999@gmail.com>
2026-09-05 23:18:51 +0300

    Update MainActivity.java

--- app/src/main/java/com/BattleGrounds/GfxTool/MainActivity.java
+++ app/src/main/java/com/BattleGrounds/GfxTool/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -24,27 +25,50 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Initialize Shizuku
+        ShizukuHelper.registerListener();
+        ShizukuHelper.initializeShizuku(this);
+
         // Set custom Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-        // Change the status bar color to match the toolbar color (if Android version is >= Lollipop)
+        // Change the status bar color to match the toolbar color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
-            window.setStatusBarColor(getResources().getColor(com.BattleGrounds.GfxTool.R.color.toolbarColor)); // Set status bar color
-            // Optional: Make the status bar translucent
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+            window.setStatusBarColor(
+                    getResources().getColor(
+                            com.BattleGrounds.GfxTool.R.color.toolbarColor
+                    )
+            );
+
+            window.addFlags(
+                    WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
+            );
+
+            window.clearFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+            );
         }
 
         // Initialize BottomNavigationView
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         // Configure Navigation
+        NavController navController = Navigation.findNavController(
+                this,
+                R.id.nav_host_fragment_activity_main
+        );
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Remove Shizuku permission listener
+        ShizukuHelper.unregisterListener();
     }
 }
